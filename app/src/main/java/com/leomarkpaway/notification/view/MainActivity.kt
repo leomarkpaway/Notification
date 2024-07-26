e377bdc0e70bc6bb73c0b20a669ea08efacc0005
leomarkpaway <leomarkpaway@gmail.com>
2024-07-26 14:44:44 +0800

    :sparkles: Create and implement notification module

--- app/src/main/java/com/leomarkpaway/notification/view/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/notification/view/MainActivity.kt
@@ -1,47 +1,27 @@
 package com.leomarkpaway.notification.view
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.leomarkpaway.notification.NotificationApp
 import com.leomarkpaway.notification.R
+import com.leomarkpaway.notification.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button)
-
-        val channelID = "MyChannelID"
-        val channelName = "MyChannelName"
-        createChannel(channelID, channelName)
 
         val notificationId = 0
-        button.setOnClickListener {
-            createNotification(channelID, notificationId, "My title", "My content")
+        val title = "My title"
+        val message = "My message"
+        val notificationManager = NotificationApp.appModule.notification.notificationManager()
+        val notification = NotificationApp.appModule.notification.notification(title, message)
+        binding.btnNotification.setOnClickListener {
+            notificationManager?.notify(notificationId, notification)
         }
 
     }
-}
-
-fun Context.createChannel(channelID: String, channelName: String) {
-    val channel = NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_DEFAULT)
-    val manager = ContextCompat.getSystemService(this, NotificationManager::class.java)
-    manager?.createNotificationChannel(channel)
-}
-
-fun Context.createNotification(channelID: String, notificationId: Int,title: String, content: String) {
-    val notification = NotificationCompat.Builder(this, channelID)
-        .setContentTitle(title)
-        .setContentText(content)
-        .setSmallIcon(R.mipmap.ic_launcher)
-        .setAutoCancel(true)
-        .build()
-
-    val manager = ContextCompat.getSystemService(this, NotificationManager::class.java)
-    manager?.notify(notificationId, notification)
 }
