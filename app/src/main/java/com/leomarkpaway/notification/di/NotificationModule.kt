d823822d0baf7a35315d94332554fd324da0a6fe
leomarkpaway <leomarkpaway@gmail.com>
2024-07-26 15:31:59 +0800

    :sparkles: Add notification with intent action

--- app/src/main/java/com/leomarkpaway/notification/di/NotificationModule.kt
+++ app/src/main/java/com/leomarkpaway/notification/di/NotificationModule.kt
@@ -3,14 +3,23 @@ package com.leomarkpaway.notification.di
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.leomarkpaway.notification.R
 
 interface NotificationModule {
+    val notificationBuilder: NotificationCompat.Builder
     fun notificationManager(): NotificationManager?
     fun notification(title: String, message: String): Notification
+    fun notificationWithIntentActivity(
+        title: String,
+        message: String,
+        intent: Intent,
+        actionName: String
+    ): Notification
 }
 
 class NotificationModuleImpl(
@@ -18,6 +27,9 @@ class NotificationModuleImpl(
     private val channelID: String,
     private val channelName: String
 ) : NotificationModule {
+
+    override val notificationBuilder: NotificationCompat.Builder =
+        NotificationCompat.Builder(appContext, channelID)
 
     override fun notificationManager(): NotificationManager? {
         val channel = NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_DEFAULT)
@@ -27,11 +39,22 @@ class NotificationModuleImpl(
     }
 
     override fun notification(title: String, message: String): Notification {
-        return NotificationCompat.Builder(appContext, channelID)
+        return notificationBuilder.setContentTitle(title).setContentText(message).build()
+    }
+
+    override fun notificationWithIntentActivity(
+        title: String,
+        message: String,
+        intent: Intent,
+        actionName: String
+    ): Notification {
+        val pendingIntent = PendingIntent.getActivity(appContext, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        return notificationBuilder
             .setContentTitle(title)
             .setContentText(message)
             .setSmallIcon(R.mipmap.ic_launcher)
             .setAutoCancel(true)
+            .addAction(0, actionName, pendingIntent)
             .build()
     }
 
