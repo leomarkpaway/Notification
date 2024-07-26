ea1c0131c320fd45a27323ec3c645012988d27b5
leomarkpaway <leomarkpaway@gmail.com>
2024-07-26 16:37:41 +0800

    :hammer: Clean up code and implement view model

--- app/src/main/java/com/leomarkpaway/notification/di/NotificationModule.kt
+++ app/src/main/java/com/leomarkpaway/notification/di/NotificationModule.kt
@@ -1,25 +1,14 @@
 package com.leomarkpaway.notification.di
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
-import com.leomarkpaway.notification.R
 
 interface NotificationModule {
-    val notificationBuilder: NotificationCompat.Builder
-    fun notificationManager(): NotificationManager?
-    fun notification(title: String, message: String): Notification
-    fun notificationWithIntentActivity(
-        title: String,
-        message: String,
-        intent: Intent,
-        actionName: String
-    ): Notification
+    val builder: NotificationCompat.Builder
+    val manager: NotificationManager?
 }
 
 class NotificationModuleImpl(
@@ -28,35 +17,16 @@ class NotificationModuleImpl(
     private val channelName: String
 ) : NotificationModule {
 
-    override val notificationBuilder: NotificationCompat.Builder =
+    override val builder: NotificationCompat.Builder =
         NotificationCompat.Builder(appContext, channelID)
 
-    override fun notificationManager(): NotificationManager? {
+    override val manager = createNotificationManager()
+
+    private fun createNotificationManager() : NotificationManager? {
         val channel = NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_DEFAULT)
         val manager = ContextCompat.getSystemService(appContext, NotificationManager::class.java)
         manager?.createNotificationChannel(channel)
         return manager
-    }
-
-    override fun notification(title: String, message: String): Notification {
-        return notificationBuilder.setContentTitle(title).setContentText(message)
-            .setSmallIcon(R.mipmap.ic_launcher).build()
-    }
-
-    override fun notificationWithIntentActivity(
-        title: String,
-        message: String,
-        intent: Intent,
-        actionName: String
-    ): Notification {
-        val pendingIntent = PendingIntent.getActivity(appContext, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-        return notificationBuilder
-            .setContentTitle(title)
-            .setContentText(message)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setAutoCancel(true)
-            .addAction(0, actionName, pendingIntent)
-            .build()
     }
 
 }
--- app/src/main/java/com/leomarkpaway/notification/view/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/notification/view/MainActivity.kt
@@ -1,40 +1,40 @@
 package com.leomarkpaway.notification.view
 
+import android.app.PendingIntent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.leomarkpaway.notification.NotificationApp
 import com.leomarkpaway.notification.R
-import com.leomarkpaway.notification.common.utils.intentActivity
+import com.leomarkpaway.notification.common.utils.pendingIntentActivity
 import com.leomarkpaway.notification.databinding.ActivityMainBinding
+import com.leomarkpaway.notification.viewmodel.MainViewModel
+import com.leomarkpaway.notification.viewmodel.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val notificationId = 0
-        val title = "My title"
-        val message = "My message"
-        val notificationModule = NotificationApp.appModule.notification
-        val manager = notificationModule.notificationManager()
+        viewModel = ViewModelProvider(
+            viewModelStore,
+            MainViewModelFactory(NotificationApp.appModule.notification)
+        )[MainViewModel::class.java]
 
         binding.btnNotification.setOnClickListener {
-            val notification = notificationModule.notification(title, message)
-            manager?.notify(notificationId, notification)
+            val title = "simple notification"
+           viewModel.showSimpleNotification(0, title)
         }
 
         binding.btnNotifWithIntentActivity.setOnClickListener {
-            val intentMain = intentActivity<MainActivity>()
-            val notification = notificationModule.notificationWithIntentActivity(
-                title,
-                message,
-                intentMain,
-                "Open Activity"
-            )
-            manager?.notify(notificationId, notification)
+            val title = "notification with action"
+            val actionName = "Open Activity"
+            val pendingIntent =
+                pendingIntentActivity<MainActivity>(0, PendingIntent.FLAG_IMMUTABLE, {})
+            viewModel.showNotificationWithAction(1, title, pendingIntent, actionName)
         }
 
     }
